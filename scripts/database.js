5ef6bf6002e576012b3302526919dafbbb4c36f6
sorachung <sorachung.src@gmail.com>
2021-12-09 13:36:23 -0600

    added save ability for journal entries

--- scripts/database.js
+++ scripts/database.js
@@ -10,6 +10,9 @@ const database = {
     "entries": []
 }
 
+const API = `http://localhost:8088`
+const mainContainer = document.querySelector("#content");
+
 /*
     You export a function that provides a version of the
     raw data in the format that you want
@@ -21,10 +24,33 @@ export const getJournalEntries = () => {
 
 
 export const getEntries = () => {
-    return fetch("http://localhost:8088/entries") // Fetch from the API
+    return fetch(`${API}/entries`) // Fetch from the API
         .then(response => response.json())  // Parse as JSON
         .then(entries => { 
             database.entries = entries;
             // What should happen when we finally have the array?
         })
+}
+
+export const saveJournalEntry (newJournalEntry) => {
+    // Use `fetch` with the POST method to add your entry to your API
+    fetch(`${API}/entries`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newJournalEntry)
+    })
+        .then(
+            (response) => {
+                //  Get all journal entries
+                response.json()
+            }
+        )
+        .then(
+            () => {
+                //  Broadcast the state change event
+                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
 }
