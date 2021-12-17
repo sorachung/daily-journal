53a628275d8c43caf8dd4b87e50b949777c92252
sorachung <sorachung.src@gmail.com>
2021-12-17 10:02:20 -0600

    made filter area in index.html and redid main content area:

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,12 @@
 import { DailyJournal } from "./DailyJournal.js";
 import { fetchEntries, fetchMoods } from "./dataAccess.js";
 
-const container = document.querySelector(".container");
+const container = document.querySelector(".container")
+const mainContentContainer = document.querySelector(".mainContent");
 
 const render = () => {
   Promise.all([fetchEntries(), fetchMoods()])
-    .then(() => container.innerHTML = DailyJournal())
+    .then(() => mainContentContainer.innerHTML = DailyJournal())
 };
 
 render();
